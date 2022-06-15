a140ff83754773d5be357caf5f630e109bf9cfcc
bers <muzafferkonal@hotmail.com>
2022-06-15 12:57:50 +0300

    Initial Commit

---
+++ week4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week4
+//
+//  Created by Muzaffer Berşan Konal on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
